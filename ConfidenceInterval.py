68a96c64afd4f24d52d7462625e3e30c6752ea7f
Clara Henry <clara.henry@mpimet.mpg.de>
2022-07-25 16:37:06 +0200

    Add result output

--- ConfidenceInterval.py
+++ ConfidenceInterval.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python
+	#!/usr/bin/python
 
 # This code calculates the confidence interval for two sets of binary data
 
 from scipy.stats import t
-from scipy.stats import zscore
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
 
-# As input, the function uses the sample sizes n1 and n2, and the number of positive outcomes (np1 and np2) within each sample population.
+# As input, the function uses the sample sizes n1 and n2, and the number of positive outcomes (np1 and np2) within each sample population. p gives the desired confidence interval.
 
 def ConfInt(np1, np2, n1, n2, p):
 
-	s12 = np.sqrt((np1 * (1 - np1))/n1)
-	s22 = np.sqrt((np2 * (1 - np2))/n2)
-	
-	Sp = np.sqrt(( s12 * (n1 - 1.0)**2 + s22 * (n2 - 1.0)**2 ) / (n1 + n2 - 2))
+	p1 = np1 / n1
+	p2 = np2 / n2	
+
+	Sp = np.sqrt( (p1*(1-p1))/n1 + (p2*(1-p2))/n2 )
 
 	if ( n1 < 30 ) or ( n2 < 30 ):
-		score = t.ppf(p, n1 + n2 -2)
+		score = t.ppf( p + (1 - p) / 2 , n1 + n2 - 2 )
 	else:
-		print("Calculation of zscore needed")
+		score = norm.ppf(p + (1 - p) / 2)
 
-	lower = np1 / n1 - np2 / n2 - score * Sp * np.sqrt( 1.0 / n1 + 1.0 / n2)
-	upper = np1 / n1 - np2 / n2 + score * Sp * np.sqrt( 1.0 / n1 + 1.0 / n2)
+	lower = (p2 - p1) - score * Sp
+	upper = (p2 - p1) + score * Sp
 	
-	meandiff = np1 - np2
+	meandiff = p1 - p2
 
-	return "( " + str(lower) + ", " + str(upper) + ")."
+	return meandiff, [ lower, upper ]
--- Tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-# ConfInt( np1, np2, n1, n2, p )
-# Here, np1 and np2 are the proportion of "True"s in the question, n1 and n2 are the sample sizes and p is for the calculation of the confidence interval (e.g. 90%).
-
-from ConfidenceInterval import ConfInt
-
-print(ConfInt(16.0 / 79.0, 9.0 / 10.0, 79, 10, 0.95))
-
