d883031472b641fa680ece2c95e8e922436e190f
Clara Henry <clara.henry@mpimet.mpg.de>
2022-08-08 10:44:22 +0200

    Round to two decimal places

--- ConfidenceInterval.py
+++ ConfidenceInterval.py
@@ -19,12 +19,12 @@ def ConfInt(np1, np2, n1, n2, p):
 
 	if ( n1 < 30 ) or ( n2 < 30 ):
 		score = t.ppf( p + (1 - p) / 2 , n1 + n2 - 2 )
-		tscore = score
+		tscore = round(score,2)
 		zscore = "N/A"
 	else:
 		score = norm.ppf(p + (1 - p) / 2)
 		tscore = "N/A"
-		zscore = score
+		zscore = round(score,2)
 
 	lower = (p2 - p1) - score * Sp
 	upper = (p2 - p1) + score * Sp
@@ -40,6 +40,6 @@ def ConfInt(np1, np2, n1, n2, p):
 
 	meandiff = p1 - p2
 
-	CI = [lower, upper]
+	CI = [round(lower,2), round(upper,2)]
 
-	return "The difference between the means is " + str(meandiff) + ". There is a " + str(int(p*100)) + "% chance that the confidence interval of " + str([lower, upper]) + " contains the true difference in the proportion of people who answered 'yes' between those who took the First Aid Course less than and more than 12 months previously. " + statement, tscore, zscore, meandiff, CI, sigdiff 
+	return "The difference between the means is " + str(meandiff) + ". There is a " + str(int(p*100)) + "% chance that the confidence interval of " + str([lower, upper]) + " contains the true difference in the proportion of people who answered 'yes' between those who took the First Aid Course less than and more than 12 months previously. " + statement, tscore, zscore, round(meandiff,2), CI, sigdiff 
